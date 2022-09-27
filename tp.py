ad934d2de794405261ad8c848a6f7f6aee6b3cca
dama <amadama931@gmail.com>
2022-09-27 12:19:38 +0200

    first commit

---
+++ tp.py
@@ -0,0 +1,48 @@
+my_int = 1 #Un nombre entier
+my_réel = 1.5 #Un nombre réel
+my_chaine_caractères = "hello world" #Une chaine de caractères
+my_bool = True # Un booleen
+#print (my_int,my_réel,my_chaine_caractères)
+#print (type(my_int),type(my_réel),type(my_chaine_caractères))
+#print (type(my_bool),type(my_int),type(my_réel),type(my_chaine_caractères))
+#exercice d'affectation
+A = 5
+B = 6
+#print ("A =",A,"B = ",B)
+#On permute
+C = A
+A = B
+B = C
+#print ("A =",A, "B =",B, "C=",C)
+#les maths
+count = 10000 + 50000
+#print (count)
+# exponentiel(puissance) = **
+# modulo (fraction) = %
+# addition = +
+# multiplication = *
+# soustraction = -
+# division = /
+# (print "A=", A)
+ResBool = (100==(3*50))
+#print (ResBool)
+A = False and False
+B = -(-(-(-2)))==-2 and 4>= 16**0.5
+C = 19% 4 != 300/10/10 and False
+D =-(1**2)<2**0 and 10% 10<=20-10*2
+#print(A,B,C,D)
+A = not True
+B = not 3**4 < 4**3
+C = not 10%3 <= 10%2
+D = not 3**2 + 4**2 != 5**2
+E = not not False
+#print (A,B,C,D,E)
+bool = True or not False and False
+#print (bool)
+#instruction IF
+a = input()
+a = int()
+if (a == 0):
+    print ("a est égale à 0")
+else:
+    print ("a n'est pas égale à 0")   
